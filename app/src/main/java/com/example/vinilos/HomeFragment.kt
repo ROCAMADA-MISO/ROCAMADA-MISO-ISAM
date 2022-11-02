8e4384912d2831d7682107a0c0563424086b25e6
Camilo Sanchez <ca.sanchezc12@uniandes.edu.co>
2022-11-01 21:42:24 -0500

    Create award

--- app/src/main/java/com/example/vinilos/HomeFragment.kt
+++ app/src/main/java/com/example/vinilos/HomeFragment.kt
@@ -1,12 +1,12 @@
 package com.example.vinilos
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.vinilos.databinding.FragmentHomeBinding
 
 
@@ -26,7 +26,18 @@ class HomeFragment : Fragment() {
     ): View? {
         val binding: FragmentHomeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home,container,false)
         binding.createAwardButton.setOnClickListener (Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_createAwardFragment))
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!, requireView().findNavController()) ||
+        super.onOptionsItemSelected(item)
     }
 
 }
