c318d8916281584329d6ca5e45a821e66f020fa5
Camilo Sánchez <ca.sanchezcr12@uniandes.edu.co>
2022-11-05 09:45:35 -0500

    Conexión de botones

---
+++ app/src/main/java/com/example/vinilos/ui/HomeFragment.kt
@@ -0,0 +1,34 @@
+package com.example.vinilos
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.example.vinilos.databinding.HomeFragmentBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TitleFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding: HomeFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.home_fragment,container,false)
+        binding.bandListButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_bandFragment))
+        binding.musicianListButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_musicianFragment))
+        binding.prizesList.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_prizeFragment))
+
+        return binding.root
+    }
+
+}
