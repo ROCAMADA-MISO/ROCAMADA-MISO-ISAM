cca5371b2c4d68ac7369e2ce7f1e8bbefec0fe53
Camilo Sanchez <casc3798@gmail.com>
2022-11-05 22:47:58 -0500

    agregar test de crear premio

---
+++ app/src/androidTest/java/com/example/vinilos/CreatePrizeTest.java
@@ -0,0 +1,47 @@
+package com.example.vinilos;
+
+import androidx.test.filters.LargeTest;
+import static org.hamcrest.Matchers.allOf;
+import com.example.vinilos.ui.MainActivity;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import static androidx.test.espresso.Espresso.onView;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class CreatePrizeTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityTestRule = new ActivityScenarioRule<MainActivity>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction prizeButton = onView(allOf(withId(R.id.prizesList), withText("Premios")));
+        prizeButton.perform(click());
+
+        ViewInteraction createPrizeButton = onView(allOf(withId(R.id.prize_form_button)));
+        createPrizeButton.perform(click());
+
+        ViewInteraction nameField = onView(allOf(withId(R.id.awardNameEditText)));
+        nameField.perform(typeText("Premio a mejor artista independiente"));
+
+        ViewInteraction organizationField = onView(allOf(withId(R.id.awardOrganizationEditText)));
+        organizationField.perform(typeText("Grammy's"));
+
+        ViewInteraction descriptionField = onView(allOf(withId(R.id.awardDescriptionEditText)));
+        descriptionField.perform(typeText("Este es un premio al mejor artista independiente del anio 2022"));
+
+        ViewInteraction confirmCreatePrizeButton = onView(allOf(withId(R.id.create_prize_button)));
+        confirmCreatePrizeButton.perform(click());
+    }
+}
