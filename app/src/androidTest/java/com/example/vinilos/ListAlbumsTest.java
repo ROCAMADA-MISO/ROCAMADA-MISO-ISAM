46a77018159e21e7a3a4c70f4f31567cf6ade53f
Roberto-Parra <w.parrav@uniandes.edu.co>
2022-11-06 21:18:51 -0300

    se agrega test albums

---
+++ app/src/androidTest/java/com/example/vinilos/ListAlbumsTest.java
@@ -0,0 +1,36 @@
+package com.example.vinilos;
+
+import androidx.test.filters.LargeTest;
+import static org.hamcrest.Matchers.allOf;
+import com.example.vinilos.ui.MainActivity;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import static androidx.test.espresso.Espresso.onView;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class ListAlbumsTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityTestRule = new ActivityScenarioRule<MainActivity>(MainActivity.class);
+
+    @Test
+    public void listMusicians() throws InterruptedException {
+        ViewInteraction albumsButton = onView(allOf(withId(R.id.albumsListButton)));
+        albumsButton.perform(click());
+        Thread.sleep(10000);
+        onView(withText("Poeta del pueblo")).check(matches(isDisplayed()));
+    }
+}
+
