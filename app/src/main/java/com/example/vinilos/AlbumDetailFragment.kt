0c2aa1ab85add4463cacc38b7ddba56b08ab1628
Roberto <w.parrav@uniandes.edu.co>
2022-11-19 19:03:57 -0300

    Se agrega fragmento detalle album

---
+++ app/src/main/java/com/example/vinilos/AlbumDetailFragment.kt
@@ -0,0 +1,31 @@
+package com.example.vinilos
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AlbumDetailFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = AlbumDetailFragment()
+    }
+
+    private lateinit var viewModel: AlbumDetailViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.album_detail_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(AlbumDetailViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/vinilos/AlbumDetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.vinilos
+
+import androidx.lifecycle.ViewModel
+
+class AlbumDetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
