d27da48d11bfbc899a7d65a86a7e7abd7fda9ccd
Daniela Castellanos <ld.castellanos@uniandes.edu.co>
2022-11-19 08:26:12 -0500

    Artist detail styles

--- app/src/main/java/com/example/vinilos/models/Musician.kt
+++ app/src/main/java/com/example/vinilos/models/Musician.kt
@@ -1,20 +1,11 @@
 package com.example.vinilos.models
 
-import android.os.Parcelable
-import com.squareup.moshi.Json
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
-data class Musician(
+data class Musician (
     val id: Int,
     val name: String,
     val image: String,
-    val birthDate: String?
-) : Parcelable {
-    constructor(
-        id: Int,
-        name: String,
-        image: String,
-        birthDate: String?,
-    ) : this(id, name, image)
-}
+    val description: String,
+    val birthDate: String,
+    val albums:Array<Album>,
+)
--- app/src/main/java/com/example/vinilos/ui/MusicianDetailFragment.kt
+++ app/src/main/java/com/example/vinilos/ui/MusicianDetailFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 import android.widget.Toast
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import com.example.vinilos.databinding.MusicianDetailFragmentBinding
